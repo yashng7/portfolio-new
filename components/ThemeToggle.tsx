f09a2b3f7392e15463f951c4caacce1d627990ea
YashwantGawande <gawandeyashwant7@gmail.com>
2023-10-10 21:47:39 +0530

    add theme-toggle button's

---
+++ components/ThemeToggle.tsx
@@ -0,0 +1,28 @@
+"use client"
+
+import { MoonIcon, SunIcon } from "@radix-ui/react-icons"
+import { useTheme } from "next-themes"
+
+import { Button } from "@/components/ui/button"
+
+export function ThemeToggle() {
+  const { setTheme, theme } = useTheme()
+
+  return (
+    <Button
+      variant="ghost"
+      size="icon"
+      onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+    >
+      <SunIcon
+        className="w-5 h-5 transition-all scale-100 rotate-0 dark:-rotate-90 dark:scale-0"
+        aria-hidden="true"
+      />
+      <MoonIcon
+        className="absolute w-5 h-5 transition-all scale-0 rotate-90 dark:rotate-0 dark:scale-100"
+        aria-hidden="true"
+      />
+      <span className="sr-only">Toggle theme</span>
+    </Button>
+  )
+}
