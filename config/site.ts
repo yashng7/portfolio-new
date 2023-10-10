4204fd7993a7484328f0d21a10b21ed5608dda55
YashwantGawande <gawandeyashwant7@gmail.com>
2023-10-10 21:33:19 +0530

    add site config

---
+++ config/site.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+  name: "yashng/portfolio",
+  url: "" /*url of the site after deployment*/,
+  ogImage: "yashng.png",
+  description:
+    "Portfolio website for yashng build with Next.js, contentlayer and shadcn/ui",
+  links: {
+    twitter: "https://twitter.com/yashng7",
+    github: "https://github.com/yashng7/portfolio-new",
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
