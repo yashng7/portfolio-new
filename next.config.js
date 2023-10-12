68f339eacc71db2d45f88c546b06310fee8626b4
YashwantGawande <gawandeyashwant7@gmail.com>
2023-10-12 21:04:24 +0530

    add next-config

---
+++ next.config.js
@@ -0,0 +1,5 @@
+const { withContentlayer } = require("next-contentlayer");
+/** @type {import('next').NextConfig} */
+const nextConfig = { reactStrictMode: true, swcMinify: true };
+
+module.exports = withContentlayer(nextConfig);
